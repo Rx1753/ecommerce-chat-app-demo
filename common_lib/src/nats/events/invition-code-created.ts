7b3bf81f9920910fb6a4cf006746f0f6d262723d
nidhi.gokani <gokaninidhi9521@gmail.com>
2022-12-14 17:59:52 +0530

    Updates

--- common_lib/src/nats/events/invition-code-created.ts
+++ common_lib/src/nats/events/invition-code-created.ts
@@ -2,7 +2,6 @@ import { Subjects } from "../enums/subjects";
 export interface InvitionCodeCreatedEvent{
     subject:Subjects.InvitionCodeCreated,
     data:{
-        id:string,
         type: string;
         phoneNumber?: Number;
         email?: string;
