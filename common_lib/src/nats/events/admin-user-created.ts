18bdf7df4e3fde34575673946bebff23059c7156
nidhi.gokani <gokaninidhi9521@gmail.com>
2022-12-16 13:18:23 +0530

    Updates

--- common_lib/src/nats/events/admin-user-created.ts
+++ common_lib/src/nats/events/admin-user-created.ts
@@ -2,6 +2,7 @@ import { Subjects } from "../enums/subjects";
 export interface AdminUserCreatedEvent{
     subject:Subjects.AdminUserCreated,
     data:{
+        id:string;
         userName: string;
         email?: string | null;
         phoneNumber?: number | null;
